239b94fe9416f955a48dd8283cb0e04409805c4f
KonstantinDoronin <konstantindoronin10@gmail.com>
2021-02-02 14:39:42 +0300

    adding old tasks

---
+++ tasks/coordinates/nominatim_geo_via address_op1.py
@@ -0,0 +1,10 @@
+#receiving geo coordinate from csv by address via Nominatim
+import pandas as pd
+from geopy.geocoders import Nominatim
+df = pd.read_csv(r'C:\Users\Admin\Desktop\data.csv', sep='\t', error_bad_lines=False)
+print(df.head())
+geolocator = Nominatim(user_agent='konstantindoronin10@gmail.com')
+latlon = df.addr.apply(lambda addr: geolocator.geocode(addr))
+df["Latitude"] = [x.latitude for x in latlon]
+df["Longitude"] = [x.longitude for x in latlon]
+df.to_excel(r'C:\Users\Admin\Desktop\test.xlsx', sheet_name='выборка', index=False)
---
+++ tasks/coordinates/nominatim_geo_via address_op2.py
@@ -0,0 +1,15 @@
+#receiving geo coordinate from xlsx by address via Nominatim op2
+from geopy.geocoders import Nominatim
+import pandas as pd
+geolocator = Nominatim(user_agent="Kazan_GP", timeout=1000)
+df = pd.read_excel(r'C:\Users\Admin\Desktop\test.xlsx', na_values='?', sheet_name='data')#подгружаем данные в данных содержатся пустые ячейки
+def eval_results(x):
+    try:
+        return (x.latitude, x.longitude)
+    except:
+        return (None, None)
+df['city_coord'] = df['Example'].apply(geolocator.geocode).apply(lambda x: eval_results(x))
+writer = pd.ExcelWriter(r'C:\Users\Admin\test_2.xlsx', engine='xlsxwriter')#записываем статистику по категорийным данным на отдельный лист
+df.to_excel(writer,'data')
+writer.save()
+print("done")
